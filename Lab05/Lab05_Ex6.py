e8813afd3c81d93c2cccfd3dcead701376d0481b
RoboTuan <testingapplication96@gmail.com>
2020-12-28 18:09:14 +0100

    Added main

--- Lab05/Lab05_Ex6.py
+++ Lab05/Lab05_Ex6.py
@@ -94,3 +94,19 @@ class InferenceService(object):
         output_body = json.dumps(output_body)
 
         return output_body
+
+
+
+if __name__ == '__main__':
+	conf = {
+		'/': {
+			'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+			#'tools.sessions.on': True
+		}
+	}
+	cherrypy.tree.mount(InferenceService(), '/', conf)
+
+	cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+	cherrypy.config.update({'server.socket_port': 8080})
+	cherrypy.engine.start()
+	cherrypy.engine.block()
