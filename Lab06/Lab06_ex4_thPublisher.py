3b1ce35498b795f85fe93d721d922b66fdf5e170
RoboTuan <testingapplication96@gmail.com>
2020-12-30 13:03:57 +0100

    lab96

--- Lab06/Lab06_ex4_thPublisher.py
+++ Lab06/Lab06_ex4_thPublisher.py
@@ -8,45 +8,44 @@ from datetime import datetime
 
 
 if __name__ == "__main__":
-	test = DoSomething("thSendForPrediction")
-	test.run()
+    test = DoSomething("thSendForPrediction")
+    test.run()
 
-	dht_device = adafruit_dht.DHT11(D4)
-
-	while True:
-
-		now = datetime.now()
-		timestamp = int(now.timestamp())
-
-		body = {
-            #identifier
-            # CHECK URL 
-            "bn": "http://192.168.1.92/",
-            #base timestamp
-            "bt": timestamp,
-            #list of events
-            "e":[
-            ]
-        }
-
-        for i in range(6):
-
-            temperature = dht_device.temperature
-            humidity = dht_device.humidity
-
-            #name, unit, timestamp, offset, value 
-            body["e"].append({"n": "temperature", "u":"cel", "t": i*10, "v":temperature})
-            body["e"].append({"n": "humidity", "u":"RH", "t": i*10, "v":humidity})
-            
+    dht_device = adafruit_dht.DHT11(D4)
 
 
-            
+    now = datetime.now()
+    timestamp = int(now.timestamp())
 
-            time.sleep(10)
+    body = {
+        #identifier
+        # CHECK URL 
+        "bn": "http://192.168.1.92/",
+        #base timestamp
+        "bt": timestamp,
+        #list of events
+        "e":[
+        ]
+    }
+
+    for i in range(6):
+
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+
+        #name, unit, timestamp, offset, value 
+        body["e"].append({"n": "temperature", "u":"cel", "t": i*10, "v":temperature})
+        body["e"].append({"n": "humidity", "u":"RH", "t": i*10, "v":humidity})
         
-        body_json = json.dumps(body)
-
-        test.myMqttClient.myPublish("/276033/th_classifier", body_json)
 
 
-	#test.end()
+        
+
+        time.sleep(10)
+    
+    body_json = json.dumps(body)
+
+    test.myMqttClient.myPublish("/276033/th_classifier", body_json)
+
+
+    test.end()
